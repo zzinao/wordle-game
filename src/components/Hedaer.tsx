ac5b46a537f74f4b3dc907f2c8e4d550b363c7e4
zzinao <77870077+zzinao@users.noreply.github.com>
2022-11-04 11:04:54 +0900

    feat: header 분리

---
+++ src/components/Hedaer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+	return(
+		<div className="m-6">
+			<h1 className="text-center text-5xl font-bold">Wordle</h1>
+		</div>
+	);
+};
+
+export default Header;
