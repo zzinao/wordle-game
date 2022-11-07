11cb67a3cff992db985a533775bd745af4881931
zzinao <77870077+zzinao@users.noreply.github.com>
2022-11-07 18:23:00 +0900

    style: 수정

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,20 +5,21 @@ const px0_200 = { ...Array.from(Array(201)).map((_, i) => `${i}px`) };
 const px0_300 = { ...Array.from(Array(301)).map((_, i) => `${i}px`) };
 const px0_700 = { ...Array.from(Array(701)).map((_, i) => `${i}px`) };
 
+// eslint-disable-next-line no-undef
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      fontSize: px0_100,
-      width: px0_700,
-      height: px0_700,
-      colors: {
-        // 메인 색깔 값 추가
-        myGreen: "#6aaa64",
-        myYellow: "#c9b458",
-        myGray: "#787c7e",
-      },
-    },
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontSize: px0_100,
+			width: px0_700,
+			height: px0_700,
+			colors: {
+				// 메인 색깔 값 추가
+				myGreen: '#6aaa64',
+				myYellow: '#c9b458',
+				myGray: '#787c7e',
+			},
+		},
+	},
+	plugins: [],
 };
