c6fb19446bc7baf1c519adced7d4f74eb3f1c2a0
zzinao <kjn0265@gmail.com>
2023-01-15 20:29:44 +0900

    remove: msw 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/globalStyle.css";
 import App from "./App";
-import { worker } from "./mocks/worker";
-
-// 개발 환경일때만 구동
-if (process.env.NODE_ENV === "development") {
-  worker.start();
-}
 
 ReactDOM.render(
   <React.StrictMode>
